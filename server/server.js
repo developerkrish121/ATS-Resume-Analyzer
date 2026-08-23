f2ffa6a609ecfb491f8d5650394bc918ba5e7d0a
Krishna Bansal <krishnabansal27august2005@gmail.com>
2026-08-23 16:51:07 +0530

    Bind Express server to Render interface

--- server/server.js
+++ server/server.js
@@ -103,7 +103,7 @@ const startServer = async () => {
     await connectDB();
 
     const port = Number(process.env.PORT) || 5000;
-    return app.listen(port, () => {
+    return app.listen(port, "0.0.0.0", () => {
       console.log(`Server running on port ${port}`);
     });
   } catch (error) {
